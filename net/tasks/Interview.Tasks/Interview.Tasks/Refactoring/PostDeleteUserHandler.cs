7970e607aa6d35fbe7c1f06306a4160c1c992d26
Nikita Mikhailov <nikitamikh2015@yandex.ru>
2023-11-14 09:45:52 +0300

    Добавил editorconfig. Добавил задание на рефакторинг

---
+++ net/tasks/Interview.Tasks/Interview.Tasks/Refactoring/PostDeleteUserHandler.cs
@@ -0,0 +1,38 @@
+﻿using UserService.Application.Interface;
+using MediatR;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace UserService.Application.CQRS.Command.PostDeletUser
+{
+    public class PostDeleteUserHandler : IRequestHandler<PostDeletUserCommanda, string>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IUserContext _userContext;
+        public string Status { get; set; }
+
+        public PostDeleteUserHandler(IUserRepository userRepository, IUserContext userContext)
+        {
+            _userRepository = userRepository;
+            _userContext = userContext;
+        }
+
+        public async Task<string> Handle(PostDeletUserCommanda request, CancellationToken cancellationToken)
+        {
+            var users = _userContext.user.FirstOrDefault();
+            if (users != null)
+            {
+                _userContext.user.Remove(users);
+                _userRepository.SaveChangesAsync();
+                return Status = "Выполнено";
+            }
+            else
+            {
+                return Status = "Ошибка:  Users = await _userRepository.GetAllAsync() ";
+            }
+
+            return Status;
+        }
+
+    }
+}
---
+++ net/tasks/Interview.Tasks/Interview.Tasks/Refactoring/Repository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Interview.Tasks.Refactoring
+{
+    class Repository
+    {
+        public DbContext context = new DbContext();
+
+        public Repository()
+        {
+        }
+
+        public async Task CreateUser(User newUser)
+        {
+            var user = context.Users.GetUserById(newUser.UserId);
+
+            if (user != null)
+            {
+                throw new Exception();
+            }
+
+            if (string.IsNullOrEmpty(newUser.UserUserName))
+            {
+                throw new Exception();
+            }
+
+            await context.Users.Insert(newUser);
+            await context.SaveAsync();
+        }
+
+        public async Task Delete(User user)
+        {
+            await context.Users.Delete(user.UserId);
+            await context.SaveAsync();
+        }
+    }
+
+    record User(int UserId, string UserName, string UserUserName);
+
+
+
+
+
+    // Вспомогательные классы
+
+
+    class DbContext
+    {
+        public UserTable Users { get; set; }
+        public Task SaveAsync()
+        {
+            Console.WriteLine("Saved.");
+            return Task.CompletedTask;
+        }
+    }
+
+    interface IUserTable
+    {
+        Task<User> GetUserById(int userId);
+        Task Insert(User user);
+        Task Delete(int userId);
+    }
+
+    class UserTable : IUserTable
+    {
+        public Task Delete(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserById(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
